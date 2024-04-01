20c23838d6f927f9b708ec06e7b47c3999b47fd5
Anuj Singh <anuj@heartitout.in>
2024-04-02 01:47:44 +0530

    new path added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import * as React from "react";
 import AppNavigation from "./src/navigation";
 import AppNavigation2 from "./src/navigation/index2";
+import AppNavigation3 from "./src/navigation/index3";
 import SInfo from "react-native-encrypted-storage";
 import * as Sentry from "@sentry/react-native";
 import {
@@ -15,8 +16,30 @@ Sentry.init({
   dsn: "https://e5adfef643df1d558d810f49f20e22a9@o4506911526813696.ingest.us.sentry.io/4506911552569344",
 });
 
+import { LogLevel, OneSignal } from "react-native-onesignal";
+
+// Add OneSignal within your App's root component
+const AppInitializer = () => {
+  console.log("App initializing");
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize("3a865120-5f7d-41a2-b5b3-5bb205884c50");
+
+  // requestPermission will show the native iOS or Android notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission(true);
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener("click", (event) => {
+    console.log("OneSignal: notification clicked:", event);
+  });
+};
+
 export default function App() {
   const [token, setToken] = React.useState(false);
+  const [welcome, setWelcome] = React.useState(false);
   const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -33,6 +56,7 @@ export default function App() {
   }, []);
 
   React.useEffect(() => {
+    AppInitializer();
     const isLogin = async () => {
       try {
         const storedToken = await SInfo.getItem("token");
@@ -41,8 +65,13 @@ export default function App() {
         } else {
           const data = JSON.parse(storedToken);
           // console.log(data)
-          if (data.status !== "true") setToken(false);
-          else setToken(true);
+          if (data.status !== "true") {
+            setToken(false);
+            const wel = await SInfo.getItem("token");
+            if (wel == undefined || wel == null) {
+              setWelcome(true);
+            }
+          } else setToken(true);
         }
       } catch (error) {
         console.error("Error while fetching token:", error);
@@ -60,7 +89,13 @@ export default function App() {
   } else {
     return (
       <AuthProvider>
-        {token ? <AppNavigation /> : <AppNavigation2 />}
+        {token ? (
+          <AppNavigation />
+        ) : welcome ? (
+          <AppNavigation3 />
+        ) : (
+          <AppNavigation2 />
+        )}
       </AuthProvider>
     );
   }
